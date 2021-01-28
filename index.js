0d5b84dcda6f0cad7e7e115332c15c0abfe79a38
jkarmarkar <jukarmarkar@gmail.com>
2021-01-28 16:34:46 -0500

    Changed index file to v2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send(' This is the v2 application ! Change propagated ')
 })
 
 app.listen(app.get('port'), function() {
